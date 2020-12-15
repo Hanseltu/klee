b7347a2e83e98a4be2351797ee83e92e60cf2bfb
Hosea <tuhaoxin6@gmail.com>
2020-12-15 14:00:06 +0800

    Update fn.c

--- examples/stack-overflow-example/fn/fn.c
+++ examples/stack-overflow-example/fn/fn.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char* argv[]) {
     // 8M = 8388608 bytes
-    char buf[8388608] = "I love !\n";
+    char buf[8388608] = "I love SMU!\n";
     printf("%s", buf);
     return 0;
 }
