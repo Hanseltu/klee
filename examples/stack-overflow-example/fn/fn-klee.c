1408638613f341ae54ffcc6748f5d2cf8188ca0e
Hosea <tuhaoxin6@gmail.com>
2020-12-15 15:17:27 +0800

    Update fn-klee.c

--- examples/stack-overflow-example/fn/fn-klee.c
+++ examples/stack-overflow-example/fn/fn-klee.c
@@ -4,7 +4,7 @@
 int main(int argc, char* argv[]) {
     int i = 0;
     // 8M = 8388608 bytes
-    char buf[8388608] = "I love DUT!\n";
+    char buf[8388608] = "I love SMU!\n";
     klee_make_symbolic(&buf, sizeof(buf), "buf");
     for (i = 0; i < sizeof(buf); i ++)
         klee_assume(buf[i] != '\x00');
